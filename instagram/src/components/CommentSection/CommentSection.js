52b3ff6e6e9eec2c7bce272f44c353b2b7414a7d
unknown <kcmp711@gmail.com>
2019-03-16 12:03:48 -0400

    added react icons dependency and added AddComment.js to CommentSection

--- instagram/src/components/CommentSection/CommentSection.js
+++ instagram/src/components/CommentSection/CommentSection.js
@@ -2,23 +2,53 @@ import React, { Component } from "react";
 import PropTypes from "prop-types";
 
 import Comment from "../CommentSection/Comments.js";
+import AddComment from "../CommentSection/AddComment";
+import "./CommentSection.css";
 
 class CommentSection extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      comments: this.props.comments
+      comments: this.props.comments,
+      commentText: ""
     };
   }
+
+  //adds new comment and passes it up to App
+  addNewCommment = (event, index) => {
+    event.preventDefault();
+    const comment = this.state.commentText;
+    this.props.updateComments(this.props.index, comment);
+    this.setState({ commentText: "" });
+  };
+
+  //handles state for AddComment in this component
+  changeHandler = event => {
+    this.setState({ [event.target.name]: event.target.value });
+  };
+
   render() {
     console.log(this.state.comments);
     return (
       <div className="comment-section-container">
-        {this.state.comments.map((comment, i) => {
-          return (
-            <Comment key={i} username={comment.username} text={comment.text} />
-          );
-        })}
+        <div className="comments">
+          {this.state.comments.map((comment, i) => {
+            return (
+              <Comment
+                key={i}
+                username={comment.username}
+                text={comment.text}
+              />
+            );
+          })}
+        </div>
+        <p className="time-stamp">{this.props.timestamp}</p>
+        <hr className="comment-divider" />
+        <AddComment
+          // addNewCommment={this.addNewCommment}
+          changeHandler={this.changeHandler}
+          commentText={this.state.commentText}
+        />
       </div>
     );
   }
